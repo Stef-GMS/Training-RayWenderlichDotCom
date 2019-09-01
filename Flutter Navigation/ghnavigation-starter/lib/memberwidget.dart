0721682e10d2a0f422710ac1b3f78496209e8efc
Stef-GMS <GeekMeSpeak@gmail.com>
2019-09-01 15:52:31 -0700

    After Returning a Value

--- Flutter Navigation/ghnavigation-starter/lib/memberwidget.dart
+++ Flutter Navigation/ghnavigation-starter/lib/memberwidget.dart
@@ -29,14 +29,86 @@ class MemberState extends State<MemberWidget> {
               ),
               onPressed: () {
                 Navigator.pop(context);
-              },
+              }, //onPressed
+            ),
+            RaisedButton(
+              child: Text("PRESS ME"),
+              onPressed: () {
+                _showOKScreen(context);
+              }, // onPressed
             ),
           ],
         ),
       ),
     );
-  }
-}
+  } // build
+
+  _showOKScreen(BuildContext context) async {
+    // 1 Push a new MaterialPageRoute onto the stack,
+    // this time with a type parameter of bool. The type
+    // parameter denotes the type you want to return when going back.
+
+    // 2 Use await when pushing the new route, which
+    // waits until the route is popped.
+    bool value = await Navigator.push(
+      context,
+      MaterialPageRoute<bool>(
+        builder: (BuildContext context) {
+          return Padding(
+            padding: const EdgeInsets.all(32.0),
+
+            // 3 The route you push onto the stack has a Column
+            // that shows two text widgets with gesture detectors.
+            child: Column(
+              children: [
+                // 4 Tapping on the text widgets causes calls to
+                // Navigator to pop the new route off the stack.
+
+                // 5 In the calls to pop(), you pass a return value
+                // of true if the user tapped the “OK” text on the
+                // screen, and false if the user tapped “NOT OK”.
+                // If the user presses the back button instead, the
+                // value returned is null.
+
+                GestureDetector(
+                    child: Text('OK'),
+                    onTap: () {
+                      Navigator.pop(context, true);
+                    }),
+                GestureDetector(
+                  child: Text('NOT OK'),
+                  onTap: () {
+                    Navigator.pop(context, false);
+                  },
+                )
+              ], // children
+            ),
+          );
+        }, //builder: (BuildContext context)
+      ),
+    ); // await Navigator.push
+
+    // 6 Create an AlertDialog to show the result returned from the route.
+    var alert = AlertDialog(
+      content: Text((value != null && value)
+          ? "OK was pressed"
+          : "NOT OK or BACK was pressed"),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('OK'),
+          onPressed: () {
+            Navigator.pop(
+                context); // 7 the AlertDialog itself must be popped off the stack.
+          },
+        )
+      ],
+    );
+
+    // 8 call showDialog() to show the alert.
+    showDialog(context: context, child: alert);
+  } // _showOKScreen
+
+} // MemberState
 
 class MemberWidget extends StatefulWidget {
   // 1 Add a Member property for the widget.
