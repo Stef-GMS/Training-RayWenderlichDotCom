61301a5ce0ffb88703851811301e11220ddd0744
Stef-GMS <GeekMeSpeak@gmail.com>
2019-09-06 14:17:32 -0700

    Initial commit

---
+++ Getting Started with the BLoC Pattern/Restaurant-Finder/restaurant-finder-final/lib/BLoC/bloc_provider.dart
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2019 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:restaurant_finder/BLoC/bloc.dart';
+
+// 1
+class BlocProvider<T extends Bloc> extends StatefulWidget {
+  final Widget child;
+  final T bloc;
+
+  const BlocProvider({Key key, @required this.bloc, @required this.child})
+      : super(key: key);
+
+  // 2
+  static T of<T extends Bloc>(BuildContext context) {
+    final type = _providerType<BlocProvider<T>>();
+    final BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    return provider.bloc;
+  }
+
+  // 3
+  static Type _providerType<T>() => T;
+
+  @override
+  State createState() => _BlocProviderState();
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+  // 4
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  // 5
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+}
---
+++ Getting Started with the BLoC Pattern/Restaurant-Finder/restaurant-finder-final/lib/UI/restaurant_screen.dart
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2019 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:restaurant_finder/BLoC/bloc_provider.dart';
+import 'package:restaurant_finder/BLoC/restaurant_bloc.dart';
+import 'package:restaurant_finder/DataLayer/location.dart';
+import 'package:restaurant_finder/DataLayer/restaurant.dart';
+import 'package:restaurant_finder/UI/favorite_screen.dart';
+import 'package:restaurant_finder/UI/location_screen.dart';
+import 'package:restaurant_finder/UI/restaurant_tile.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  final Location location;
+
+  const RestaurantScreen({Key key, @required this.location}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(location.title),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.favorite_border),
+            onPressed: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => FavoriteScreen())),
+          )
+        ],
+      ),
+      body: _buildSearch(context),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.edit_location),
+        onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => LocationScreen(
+                  // 1
+                  isFullScreenDialog: true,
+                ),
+            fullscreenDialog: true)),
+      ),
+    );
+  }
+
+  Widget _buildSearch(BuildContext context) {
+    final bloc = RestaurantBloc(location);
+
+    return BlocProvider<RestaurantBloc>(
+      bloc: bloc,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'What do you want to eat?'),
+              onChanged: (query) => bloc.submitQuery(query),
+            ),
+          ),
+          Expanded(
+            child: _buildStreamBuilder(bloc),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStreamBuilder(RestaurantBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.stream,
+      builder: (context, snapshot) {
+        final results = snapshot.data;
+
+        if (results == null) {
+          return Center(child: Text('Enter a restaurant name or cuisine type'));
+        }
+
+        if (results.isEmpty) {
+          return Center(child: Text('No Results'));
+        }
+
+        return _buildSearchResults(results);
+      },
+    );
+  }
+
+  Widget _buildSearchResults(List<Restaurant> results) {
+    return ListView.separated(
+      itemCount: results.length,
+      separatorBuilder: (context, index) => Divider(),
+      itemBuilder: (context, index) {
+        final restaurant = results[index];
+        return RestaurantTile(restaurant: restaurant);
+      },
+    );
+  }
+}
